aec5ab783bfdb2a261daefe35d971522b73af325
Utkarsh Mankad <utkarsh.mankad@gmail.com>
2021-03-28 16:03:14 +0530

    Lifting state up

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,7 +6,10 @@ class NavBar extends Component {
     return (
       <nav className="navbar navbar-light bg-light">
         <a className="navbar-brand" href="#">
-          Navbar
+          Total Counters
+          <span className="badge badge-pill badge-secondary m-2">
+            {this.props.totalCounters}
+          </span>
         </a>
       </nav>
     );
