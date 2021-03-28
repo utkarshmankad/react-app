7338725087bb69a28ed3b383f10f5e7b1d923359
utkarshmankad <utkarsh.mankad@gmail.com>
2021-03-28 05:27:15 +0000

    committing changes from gitpod branch sun

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,26 +2,26 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 5 },
-      { id: 3, value: 1 },
-      { id: 4, value: 0 },
-      { id: 5, value: 7 },
-    ],
-  };
-  render() {
-    return (
-      <div>
-        {this.state.counters.map((counter) => (
-          <Counter key={counter.id} value={counter.value} selected={true}>
-            <h5>Counter {counter.id}</h5>
-          </Counter>
-        ))}
-      </div>
-    );
-  }
+    state = {
+        counters: [
+            { id: 1, value: 4 },
+            { id: 2, value: 5 },
+            { id: 3, value: 1 },
+            { id: 4, value: 0 },
+            { id: 5, value: 7 },
+        ],
+    };
+    render() {
+        return (
+            <div>
+                {this.state.counters.map((counter) => (
+                    <Counter key={counter.id} value={counter.value} selected={true}>
+                        <h5>Counter {counter.id}</h5>
+                    </Counter>
+                ))}
+            </div>
+        );
+    }
 }
 
 export default Counters;
